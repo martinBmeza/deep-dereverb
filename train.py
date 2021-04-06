40d6d6d57acffbb55e1ca6e0c9e93113c22b9467
martinBmeza <martinbernardomeza@gmail.com>
2021-04-06 16:13:10 -0300

    tensorboard trackeando batches

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ Bucle de entrenamiento
 """
 
 import sys
+import tensorflow as tf
 MAIN_PATH="/home/martin/Documents/tesis/src"
 sys.path.append(MAIN_PATH) #Para poder importar archivos .py como librerias
 
@@ -21,7 +22,12 @@ modelo.summary()
 
 #callbacks
 
+cbks = [tf.keras.callbacks.EarlyStopping(monitor='loss',restore_best_weights=True),
+        tf.keras.callbacks.ModelCheckpoint('/home/martin/Documents/tesis/src/model/ckpts/weights.{epoch:02d}-{loss:.2f}.hdf5'),
+        tf.keras.callbacks.TensorBoard(log_dir='tb_logs',profile_batch=0, update_freq='batch', histogram_freq=1)]
+
+
 
 #entrenando                                                                                                                                                                 
-history = modelo.fit(training_generator, workers=6, epochs= 10)
+history = modelo.fit(training_generator, callbacks=cbks, workers=10, epochs=10)
 
